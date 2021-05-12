a8a60218d7d08527e38a9d9bc870b6e8396be427
abhishek <abhisingh.489113@gmail.com>
2021-05-12 14:15:26 +0530

    initial commit

---
+++ home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'variable':"this is sent"
+    }
+    return render(request,'index.html', context)
+
+def about(request):
+    return render(request,'about.html')
+    
+
+def service(request):
+    return render(request,'services.html')
+
+def contact(request):
+    return render(request,'contact.html')
